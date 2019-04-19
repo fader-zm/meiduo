c2ab57eab1aca9ca882c4ba786c60c78c234d88d
mater <mater@163.com>
2019-04-19 10:30:04 +0800

    订单列表获取

--- meiduo_mall/meiduo_mall/apps/payment/views.py
+++ meiduo_mall/meiduo_mall/apps/payment/views.py
@@ -13,7 +13,7 @@ from .models import Payment
 class PaymentVIew(APIView):
     """生成支付连接"""
     permission_classes = [IsAuthenticated]
-
+    
     def get(self, request, order_id):
         # 获取当前的请求用户对象
         user = request.user
@@ -23,7 +23,7 @@ class PaymentVIew(APIView):
                                                 status=OrderInfo.ORDER_STATUS_ENUM["UNPAID"])
         except OrderInfo.DoesNotExist:
             return Response({"message": "订单不存在"}, status=status.HTTP_400_BAD_REQUEST)
-
+        
         # 创建alipay支付对象
         alipay = AliPay(
             appid=settings.ALIPAY_APPID,
@@ -36,29 +36,29 @@ class PaymentVIew(APIView):
             sign_type="RSA2",  # 加密方式RSA2
             debug=settings.ALIPAY_DEBUG  # 默认False
         )
-
+        
         # 调用SDK的方法得到支付链接后面的查询参数  拼接支付链接
         order_string = alipay.api_alipay_trade_page_pay(
             out_trade_no=order_id,  # 订单编号
-            total_amount=str(order_model.total_amount),   # 不能识别decimal 要转换类型
-            subject="美多商城%s" % order_id,  #标题
+            total_amount=str(order_model.total_amount),  # 不能识别decimal 要转换类型
+            subject="美多商城%s" % order_id,  # 标题
             return_url="http://www.meiduo.site:8080/pay_success.html"  # 支付成功后回调的url
         )
-
+        
         alipay_url = settings.ALIPAY_URL + "?" + order_string
         return Response({'alipay_url': alipay_url})
 
 
 class PaymentStatusView(APIView):
     """修改订单状态,保存支付宝交易号"""
-
+    
     def put(self, request):
         # 获取前端以查询字符串方式传入的数据
         query_dict = request.query_params
         # 需要把query_dict转成字典 把sign部分移除 再进行验证
         data = query_dict.dict()
         sign = data.pop("sign")
-
+        
         # 创建alipay对象,然后调用verify方法进行验证 是否是支付宝回传的
         alipay = AliPay(
             appid=settings.ALIPAY_APPID,
@@ -69,7 +69,7 @@ class PaymentStatusView(APIView):
             sign_type="RSA2",  # 加密方式RSA2
             debug=settings.ALIPAY_DEBUG  # 默认False
         )
-
+        
         if alipay.verify(data, sign):
             # 取出美多商城订单编号和支付宝交易号   把2个编号绑定保存到MySQL表payment
             order_id = data.get("out_trade_no")
@@ -83,6 +83,6 @@ class PaymentStatusView(APIView):
                 status=OrderInfo.ORDER_STATUS_ENUM["UNSEND"])
         else:
             return Response({"meassage": "非法请求"}, status=status.HTTP_403_FORBIDDEN)
-
+        
         # 把支付宝交易返回前端
         return Response({"trade_id": trade_no})
